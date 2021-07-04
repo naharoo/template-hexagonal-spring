9a130aa669abdfc0df97d24b978978440e62a065
naharoo <codenarek@gmail.com>
2021-07-04 23:18:28 +0400

    out directory deletion

--- out/production/resources/db/migration/V0_0_0_0__initial.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists users
-(
-    id         bigserial not null primary key,
-    first_name varchar   not null,
-    last_name  varchar   not null,
-    birthday   date      not null
-)
